258c424dea96a60c080b6fa77045b57cf51c1220
russoagustin <pickrusso.agustin@gmail.com>
2024-11-22 23:46:41 -0300

    Se cambió el nombre del método getUsuario por findById y se creó el método getUsuarios utilizado para crear un nuevo endpoint el cual devuelve la lista de todos los usuarios registrados

--- src/main/java/com/russo/api/bd2024/controllers/UsuariosController.java
+++ src/main/java/com/russo/api/bd2024/controllers/UsuariosController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -14,8 +15,14 @@ public class UsuariosController {
     UsuarioRepository repositorio = UsuarioRepository.getInstance();
 
     @GetMapping("/usuario/{id}")
-    ResponseEntity<Map<String,Object>> getUsuario(@PathVariable Integer id) throws SQLException {
-        Optional<Map<String,Object>> usuarioOptional = repositorio.getUsuario(id);
+    ResponseEntity<Map<String,Object>> findUsuario(@PathVariable Integer id) throws SQLException {
+        Optional<Map<String,Object>> usuarioOptional = repositorio.findById(id);
         return usuarioOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/usuario")
+    ResponseEntity<List<Map<String,Object>>> getUsuarios() throws SQLException {
+        Optional<List<Map<String,Object>>> listaUsuariosOptional = repositorio.getUsuarios();
+        return listaUsuariosOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/russo/api/bd2024/repository/UsuarioRepository.java
+++ src/main/java/com/russo/api/bd2024/repository/UsuarioRepository.java
@@ -1,9 +1,7 @@
 package com.russo.api.bd2024.repository;
 
 import java.sql.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class UsuarioRepository {
     private static UsuarioRepository instance;
@@ -17,12 +15,12 @@ public class UsuarioRepository {
         return instance;
     }
 
-    public Optional<Map<String,Object>> getUsuario(Integer id) throws SQLException {
+    public Optional<Map<String,Object>> findById(Integer id) throws SQLException {
         Connection con = Conexion.conexion();
         Optional<Map<String,Object>> usuarioOptional = Optional.empty();
         String stringQuery = "SELECT nombre, apellido,email, user_name,dni FROM PERSONA " +
-                "INNER JOIN USUARIO ON PERSONA.idPERSONA = USUARIO.PERSONA_idPERSONA " +
-                "INNER JOIN VIAJANTE ON PERSONA.idPERSONA = VIAJANTE.PERSONA_idPERSONA " +
+                "INNER JOIN USUARIO ON PERSONA.idPERSONA = USUARIO.PERSONA_id " +
+                "INNER JOIN VIAJANTE ON PERSONA.idPERSONA = VIAJANTE.PERSONA_id " +
                 "WHERE dni = ?";
         try {
             PreparedStatement st = con.prepareStatement(stringQuery);
@@ -42,6 +40,35 @@ public class UsuarioRepository {
             Conexion.closeConnection(con);
         }
         return usuarioOptional;
+    }
+
+    public Optional<List<Map<String,Object>>> getUsuarios() throws SQLException {
+        Connection con = Conexion.conexion();
+        List<Map<String,Object>> listaUsuarios = new ArrayList<>();
+        Optional<List<Map<String,Object>>> listaUsuariosOptional = Optional.empty();
+        Optional<Map<String,Object>> usuarioOptional = Optional.empty();
+        String stringQuery = "SELECT nombre, apellido,email, user_name,dni FROM PERSONA " +
+                "INNER JOIN USUARIO ON PERSONA.idPERSONA = USUARIO.PERSONA_id " +
+                "INNER JOIN VIAJANTE ON PERSONA.idPERSONA = VIAJANTE.PERSONA_id ";
+        try {
+            PreparedStatement st = con.prepareStatement(stringQuery);
+            ResultSet resultado = st.executeQuery();
+            while (resultado.next()) {
+                Map<String,Object> usuario = new HashMap<>();
+                usuario.put("nombre",resultado.getString("nombre"));
+                usuario.put("apellido",resultado.getString("apellido"));
+                usuario.put("email",resultado.getString("email"));
+                usuario.put("user_name",resultado.getString("user_name"));
+                usuario.put("dni",resultado.getString("dni"));
+                listaUsuarios.add(usuario);
+                usuarioOptional = Optional.of(usuario);
+            }
+            listaUsuariosOptional = Optional.of(listaUsuarios);
+            st.close();
+        } finally {
+            Conexion.closeConnection(con);
+        }
+        return listaUsuariosOptional;
     }
 
 
